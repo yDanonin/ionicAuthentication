dc85184dba00309c7c26c0339bd311c6ac8bbd8b
Fabiano Barros Rocha <73357366+yDanonin@users.noreply.github.com>
2022-07-01 12:47:28 -0300

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,13 +1,13 @@
 export const firebaseConfig = {
-    
+    // just an example
     fire : {
-    apiKey: "AIzaSyBr-bTTYe7OtTvh6QRSO8kSNSBy3fxJrvs",
-    authDomain: "ods-pam-456e4.firebaseapp.com",
-    projectId: "ods-pam-456e4",
-    storageBucket: "ods-pam-456e4.appspot.com",
-    messagingSenderId: "1031251205008",
-    appId: "1:1031251205008:web:5bc78cc34281f686a2b115",
-    measurementId: "G-DWFGBJXW1F"
+    apiKey: "AIzaSyBr-bTTYe7OtTvh6QRSO8kSNSBy",
+    authDomain: "name.firebaseapp.com",
+    projectId: "name",
+    storageBucket: "name.appspot.com",
+    messagingSenderId: "10312518",
+    appId: "1:A_LOT_OF_NUMBERS",
+    measurementId: "G-STRINGS"
     }
 
 }
